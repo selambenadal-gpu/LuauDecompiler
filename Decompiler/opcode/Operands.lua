fb01ad9bb394b3a085d8a09c16258661526a5f78
babazxığtea <156783346+selambenadal-gpu@users.noreply.github.com>
2026-08-17 10:16:44 +0300

    Update Operands.lua

--- Decompiler/opcode/Operands.lua
+++ Decompiler/opcode/Operands.lua
@@ -16,4 +16,26 @@ function Operands.E(instruction)
 	return instruction.E
 end
 
+function Operands.AUX(instruction)
+	return instruction.A, instruction.aux
+end
+
+function Operands.decode(instruction)
+	local format = instruction.format
+
+	if format == "A" then
+		return Operands.A(instruction)
+	elseif format == "ABC" then
+		return Operands.ABC(instruction)
+	elseif format == "AD" then
+		return Operands.AD(instruction)
+	elseif format == "E" then
+		return Operands.E(instruction)
+	elseif format == "AUX" then
+		return Operands.AUX(instruction)
+	end
+
+	error(("Unsupported operand format: %s"):format(tostring(format)))
+end
+
 return Operands
