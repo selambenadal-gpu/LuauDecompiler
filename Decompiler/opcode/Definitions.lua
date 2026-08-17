f8e06fbbf7719b80d15100d81236217f141c2e2b
babazxığtea <156783346+selambenadal-gpu@users.noreply.github.com>
2026-08-17 10:04:27 +0300

    Update Definitions.lua

--- Decompiler/opcode/Definitions.lua
+++ Decompiler/opcode/Definitions.lua
@@ -1,1 +1,27 @@
+local Definitions = {}
 
+Definitions.VERSION = {
+	MIN = 3,
+	MAX = 12,
+	TARGET = 9,
+}
+
+Definitions.FORMAT = {
+	ABC = "ABC",
+	AD = "AD",
+	E = "E",
+	AUX = "AUX",
+}
+
+Definitions.OPCODES = {}
+
+function Definitions.register(id, name, format, info)
+	Definitions.OPCODES[id] = {
+		id = id,
+		name = name,
+		format = format,
+		info = info,
+	}
+end
+
+return Definitions
